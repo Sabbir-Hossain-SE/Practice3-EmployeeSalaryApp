8a0d6d061d04c314e12117fc4a8f0c300309d9ad
Sabbir <sabbir.hossain133045@gmail.com>
2019-10-15 08:27:10 +0600

    Practice3

---
+++ EmployeeSalaryApp/EmployeeSalaryApp/EmployeeSalaryApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace EmployeeSalaryApp
+{
+    public partial class EmployeeSalaryApp : Form
+    {
+        public SalaryCount salarycount;
+        public EmployeeSalaryApp()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            bool flag1 = Regex.IsMatch(basicSalaryTextBox.Text, "[0-9.]{1,}$");
+            bool flag2 = Regex.IsMatch(houseRentSalaryTextBox.Text, "[0-9]{1,}$");
+            bool flag3 = Regex.IsMatch(allowanceSalaryTextBox.Text, "[0-9]{1,}$");
+            bool flag4 = Regex.IsMatch(cutomerNameTextBox.Text, "[0-9a-zA-Z]{1,}$");
+            if (flag4)
+            {
+                salarycount=new SalaryCount(cutomerNameTextBox.Text);
+            }
+            else
+            {
+                MessageBox.Show("Please input your name");
+                return;
+            }
+
+            if (flag1==true&& flag2==true&&flag3==true)
+            {
+                double basicSalary = Convert.ToDouble(basicSalaryTextBox.Text);
+                int rentSalaryPercentage = Convert.ToInt32(houseRentSalaryTextBox.Text);
+                int allowanceSalaryPercentage = Convert.ToInt32(allowanceSalaryTextBox.Text);
+               string mySalary= salarycount.ShowSalary(basicSalary, rentSalaryPercentage, allowanceSalaryPercentage);
+               MessageBox.Show(mySalary);
+            }
+            else
+            {
+                MessageBox.Show("Please enter all field properly.");
+                return;
+            }
+
+        }
+    }
+}
---
+++ EmployeeSalaryApp/EmployeeSalaryApp/SalaryCount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalaryApp
+{
+    public class SalaryCount
+    {
+        private string name;
+        private double salary;
+    
+
+    public SalaryCount(string name)
+    {
+        this.name = name;
+    }
+
+    public string ShowSalary(double basicSalary, int rentSalaryPercentage, int allowanceSalaryPercentage)
+    {
+        salary = basicSalary + basicSalary * (rentSalaryPercentage /100.00) +
+                                basicSalary * (allowanceSalaryPercentage / 100.00);
+        return name + " your salary is : " + salary;
+    }
+  }
+
+}
